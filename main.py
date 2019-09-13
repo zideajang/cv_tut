af0a58c6b4877aa869cf157ffafcf4bd2776d888
jangwoo <zidea2015@163.com>
2019-09-13 12:18:29 +0800

    draw lines and pointers

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from PIL import Image
 from pylab import *
 import os
 pil_im = Image.open('empire.jpeg')
-print(pil_im)
+# print(pil_im)
+imshow(array(pil_im))
+x = [100,100,400,400]
+y = [200,500,200,500]
+plot(x,y,'r*')
+plot(x[:2],y[:2])
+title('draw points and lines')
+show()
