ed7f7d722d3d0dfdf312542887ee5aa4241b22ae
jangwoo <zidea2015@163.com>
2019-09-13 18:44:27 +0800

    图像灰度导数

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from PIL import Image
 from pylab import *
 import numpy as np
 import os
-pil_im = Image.open('empire.jpeg').rotate(45)
+pil_im = Image.open('empire.jpeg')
 im = np.array(pil_im)
+
+im2 = 255 - im
+im3 = (100.0/255) * im - 100
+im4 = 255.0 * (im/255.0)**2
+
+figure()
 imshow(im)
+figure()
+imshow(im2)
+figure()
+imshow(im3)
+figure()
+imshow(im4)
+
 show()
