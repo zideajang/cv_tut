2706ed43024b3159079e131e1af24240382d6c4e
zhangyv <zidea@163.com>
2019-10-25 13:53:33 +0800

    提交查找边缘功能

--- img_seg_grabcut.py
+++ img_seg_grabcut.py
@@ -4,17 +4,42 @@ import numpy as np
 import argparse
 
 class App():
+
+    def find_counter(self,img):
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY) 
+        contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
+        cv2.drawContours(img,contours,-1,(0,0,255),3) 
+
+        return img
+    def threld_img_segmentation(self,img):
+
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # 高斯模糊
+        # blur =cv2.GaussianBlur(img,(5,5),0)
+        # canny 边缘检测
+        # dst = cv2.Canny(blur, 100, 200)
+        ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY) 
+        contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
+        cv2.drawContours(img,contours,-1,(0,0,255),3) 
+        return img
+
+
     def run(self,img_name):
         dir = 'images/'
         img = cv2.imread( dir +  img_name + '.jpg')
         height,width,_ = img.shape
-        scale_ratio = 4
+        scale_ratio = 2
         # (h,)
         # print(width,height)
         resize_img = cv2.resize(img,((width//scale_ratio),(height//scale_ratio)))
-        gray = cv2.cvtColor(resize_img,cv2.COLOR_BGR2GRAY)
+        
+        # 获取
+        dst = self.find_counter(resize_img)
+        
         while(1):
-            cv2.imshow("img",gray)
+            cv2.imshow("dst",dst)
+            # cv2.imshow("img",resize_img)
             key = cv2.waitKey(1)
             if key == 27:
                 break
