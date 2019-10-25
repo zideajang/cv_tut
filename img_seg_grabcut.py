9ed3778b6f0af9f72cc412ddad1680f7bb69989f
zhangyv <zidea@163.com>
2019-10-25 15:36:06 +0800

    提交分水岭

--- img_seg_grabcut.py
+++ img_seg_grabcut.py
@@ -5,6 +5,36 @@ import argparse
 
 class App():
 
+    def watershed_img_segmetation(self,img):
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        # noise removal
+        kernel = np.ones((3,3),np.uint8)
+        opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+        # sure background area
+        sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+        # Finding sure foreground area
+        dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+        ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg,sure_fg)
+
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(sure_fg)
+
+        # Add one to all labels so that sure background is not 0, but 1
+        markers = markers+1
+
+        # Now, mark the region of unknown with zero
+        markers[unknown==255] = 0
+        markers = cv2.watershed(img,markers)
+        img[markers == -1] = [255,0,0]
+        return img
+
     def find_counter(self,img):
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY) 
@@ -13,7 +43,6 @@ class App():
 
         return img
     def threld_img_segmentation(self,img):
-
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         # 高斯模糊
         # blur =cv2.GaussianBlur(img,(5,5),0)
@@ -35,7 +64,7 @@ class App():
         resize_img = cv2.resize(img,((width//scale_ratio),(height//scale_ratio)))
         
         # 获取
-        dst = self.find_counter(resize_img)
+        dst = self.watershed_img_segmetation(resize_img)
         
         while(1):
             cv2.imshow("dst",dst)
