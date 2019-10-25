da3418fb16a8a0ed12f7fef41519f01c26d95be8
zhangyv <zidea@163.com>
2019-10-25 13:12:49 +0800

    提交图片分割

--- img_seg_grabcut.py
+++ img_seg_grabcut.py
@@ -5,6 +5,19 @@ import argparse
 
 class App():
     def run(self,img_name):
+        dir = 'images/'
+        img = cv2.imread( dir +  img_name + '.jpg')
+        height,width,_ = img.shape
+        scale_ratio = 4
+        # (h,)
+        # print(width,height)
+        resize_img = cv2.resize(img,((width//scale_ratio),(height//scale_ratio)))
+        gray = cv2.cvtColor(resize_img,cv2.COLOR_BGR2GRAY)
+        while(1):
+            cv2.imshow("img",gray)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("imagename", help="display image name", type=str)
